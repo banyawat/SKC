d5ab78ac78873c8b05158e8d19543f15e3b5e2d5
bunyawat <bunyawat.38@gmail.com>
2016-06-23 14:22:27 +0700

    SKC Contact Form > Finished (Prototype)
    Just engine not UI

--- app/src/main/java/com/theteus/kubota/SKCModule/SKCForm02.java
+++ app/src/main/java/com/theteus/kubota/SKCModule/SKCForm02.java
@@ -5,17 +5,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import com.theteus.kubota.CardViewPager;
 import com.theteus.kubota.R;
 import com.theteus.kubota.RepeatAddressForm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SKCForm02 extends Fragment implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
-    TabHost mTabHost;
+    Button addrMenu1, addrMenu2, addrMenu3;
+    EditText addrNoEdit, addrNameEdit, floorEdit, roomEdit,
+            groupEdit, alleyEdit, streetEdit, subDistrictEdit,
+            districtEdit, provinceEdit, postalEdit;
+    ArrayList<addressInstance> addressList;
+    int pageID=0;
+
     public SKCForm02() {
         // Required empty public constructor
     }
@@ -25,18 +38,141 @@ public class SKCForm02 extends Fragment implements TabHost.OnTabChangeListener, 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_skcform02, container, false);
+        addressList = new ArrayList<addressInstance>();
+        addressList.add(new addressInstance());
+        addressList.add(new addressInstance());
+        addressList.add(new addressInstance());
+        findViewsById(view);
+        initButton();
 
-        /*mTabHost = (TabHost)view.findViewById(R.id.tabHost);
-        mTabHost.setup();
-        mTabHost.addTab(mTabHost.newTabSpec("Test").setIndicator("Test").setContent(android.R.id.tabcontent));
-        mTabHost.addTab(mTabHost.newTabSpec("Test").setIndicator("Test").setContent(android.R.id.tabcontent));
-        mTabHost.addTab(mTabHost.newTabSpec("Test").setIndicator("Test").setContent(android.R.id.tabcontent));*/
         return view;
+    }
+
+    private void findViewsById(View view){
+        addrMenu1 = (Button) view.findViewById(R.id.skc_form2_menu01);
+        addrMenu2 = (Button) view.findViewById(R.id.skc_form2_menu02);
+        addrMenu3 = (Button) view.findViewById(R.id.skc_form2_menu03);
+        addrNoEdit = (EditText) view.findViewById(R.id.repeat_addrnum);
+        addrNameEdit = (EditText) view.findViewById(R.id.repeat_addrvillage);
+        floorEdit = (EditText) view.findViewById(R.id.repeat_floor);
+        roomEdit = (EditText) view.findViewById(R.id.repeat_room);
+        groupEdit = (EditText) view.findViewById(R.id.repeat_group);
+        alleyEdit = (EditText) view.findViewById(R.id.repeat_alley);
+        streetEdit = (EditText) view.findViewById(R.id.repeat_street);
+        subDistrictEdit = (EditText) view.findViewById(R.id.repeat_subdistrict);
+        districtEdit = (EditText) view.findViewById(R.id.repeat_district);
+        provinceEdit = (EditText) view.findViewById(R.id.repeat_province);
+        postalEdit = (EditText) view.findViewById(R.id.repeat_postalcode);
+    }
+
+    private void initButton(){
+        addrMenu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (pageID){
+                    case 0:
+                        return;
+                    case 1:
+                        saveStatusEditText();
+                        break;
+                    case 2:
+                        saveStatusEditText();
+                        break;
+                    default: break;
+                }
+                pageID=0;
+                restoreEditText();
+            }
+        });
+
+        addrMenu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAGGY", "Teststesetset page"+pageID);
+                switch (pageID){
+                    case 0:
+                        saveStatusEditText();
+                        break;
+                    case 1:
+                        return;
+                    case 2:
+                        saveStatusEditText();
+                        break;
+                    default: break;
+                }
+                pageID=1;
+                restoreEditText();
+            }
+        });
+
+        addrMenu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (pageID){
+                    case 0:
+                        saveStatusEditText();
+                        restoreEditText();
+                        break;
+                    case 1:
+                        saveStatusEditText();
+                        restoreEditText();
+                        break;
+                    case 2:
+                        return;
+                    default: break;
+                }
+                pageID=2;
+                restoreEditText();
+            }
+        });
+    }
+
+    private void saveStatusEditText(){
+        saveEditText();
+        clearEditText();
+    }
+
+    private void restoreEditText(){
+        addressList.get(pageID).getAddressField(addrNoEdit, addrNameEdit, floorEdit, roomEdit,
+                groupEdit, alleyEdit, streetEdit, subDistrictEdit,
+                districtEdit, provinceEdit, postalEdit);
+        addrNoEdit.requestFocus();
+    }
+
+    private void saveEditText(){
+        addressList.get(pageID).setAddress(addrNoEdit, addrNameEdit, floorEdit, roomEdit,
+                groupEdit, alleyEdit, streetEdit, subDistrictEdit,
+                districtEdit, provinceEdit, postalEdit);
+    }
+    
+    private void clearEditText(){
+        addrNoEdit.setText("");
+        addrNameEdit.setText("");
+        floorEdit.setText("");
+        roomEdit.setText("");
+        groupEdit.setText("");
+        alleyEdit.setText("");
+        streetEdit.setText("");
+        subDistrictEdit.setText("");
+        districtEdit.setText("");
+        provinceEdit.setText("");
+        postalEdit.setText("");
+
+        addrNoEdit.setError(null);
+        addrNameEdit.setError(null);
+        floorEdit.setError(null);
+        roomEdit.setError(null);
+        groupEdit.setError(null);
+        alleyEdit.setError(null);
+        streetEdit.setError(null);
+        subDistrictEdit.setError(null);
+        districtEdit.setError(null);
+        provinceEdit.setError(null);
+        postalEdit.setError(null);
     }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
     }
 
     @Override
@@ -52,5 +188,44 @@ public class SKCForm02 extends Fragment implements TabHost.OnTabChangeListener, 
     @Override
     public void onTabChanged(String tabId) {
 
+    }
+
+    private class addressInstance{
+        private HashMap<String, String> addressMap;
+
+        public addressInstance(){
+            addressMap = new HashMap<String, String>();
+        }
+        public void setAddress(EditText addressNoEdit,EditText addressNameEdit,EditText floorEdit,EditText roomEdit,
+                               EditText groupEdit,EditText alleyEdit,EditText streetEdit,EditText subDistrictEdit,
+                               EditText districtEdit,EditText provinceEdit,EditText postalEdit){
+            addressMap.put("addressNo", addressNoEdit.getText().toString());
+            addressMap.put("addressName", addressNameEdit.getText().toString());
+            addressMap.put("Floor", floorEdit.getText().toString());
+            addressMap.put("Room", roomEdit.getText().toString());
+            addressMap.put("Group", groupEdit.getText().toString());
+            addressMap.put("Alley", alleyEdit.getText().toString());
+            addressMap.put("Street", streetEdit.getText().toString());
+            addressMap.put("SubDistrict", subDistrictEdit.getText().toString());
+            addressMap.put("District", districtEdit.getText().toString());
+            addressMap.put("Province", provinceEdit.getText().toString());
+            addressMap.put("Postal", postalEdit.getText().toString());
+        }
+
+        public void getAddressField(EditText addrNoEdit,EditText addrNameEdit,EditText floorEdit,EditText roomEdit,
+                                    EditText groupEdit,EditText alleyEdit,EditText streetEdit,EditText subDistrictEdit,
+                                    EditText districtEdit,EditText provinceEdit,EditText postalEdit){
+            addrNoEdit.setText(addressMap.get("addressNo"));
+            addrNameEdit.setText(addressMap.get("addressName"));
+            floorEdit.setText(addressMap.get("Floor"));
+            roomEdit.setText(addressMap.get("Room"));
+            groupEdit.setText(addressMap.get("Group"));
+            alleyEdit.setText(addressMap.get("Alley"));
+            streetEdit.setText(addressMap.get("Street"));
+            subDistrictEdit.setText(addressMap.get("SubDistrict"));
+            districtEdit.setText(addressMap.get("District"));
+            provinceEdit.setText(addressMap.get("Province"));
+            postalEdit.setText(addressMap.get("Postal"));
+        }
     }
 }
--- app/src/main/java/com/theteus/kubota/SKCModule/SKCForm03.java
+++ app/src/main/java/com/theteus/kubota/SKCModule/SKCForm03.java
@@ -3,18 +3,38 @@ package com.theteus.kubota.SKCModule;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.hudomju.swipe.OnItemClickListener;
+import com.hudomju.swipe.SwipeToDismissTouchListener;
+import com.hudomju.swipe.SwipeableItemClickListener;
+import com.hudomju.swipe.adapter.RecyclerViewAdapter;
 import com.theteus.kubota.R;
 
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SKCForm03 extends Fragment {
+    Spinner jobSpinner, goodsSpinner, agriTypeSpinner, harvestMethodSpinner, riceMethodSpinner;
+    EditText goodsAreaEdit, interestProductEdit, incomeEdit;
+    TextView limitText;
+    Button menu1Button, menu2Button, addButton;
+    ArrayList<PageInformation> pageList;
 
+    RecyclerView recyclerView;
+
+    int pageID=0;
+
+    listAdapter adapter;
 
     public SKCForm03() {
         // Required empty public constructor
@@ -24,8 +44,321 @@ public class SKCForm03 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_skcform03, container, false);
+        View view = inflater.inflate(R.layout.fragment_skcform03, container, false);
+
+        pageList = new ArrayList<PageInformation>();
+        pageList.add(new PageInformation());
+        pageList.add(new PageInformation());
+        findViewsByID(view);
+        initSpinner(view);
+        initInterestList(view);
+        initAddButton(view);
+        initMenu(view);
+
+        return view;
+    }
+
+    private void findViewsByID(View view){
+        jobSpinner = (Spinner) view.findViewById(R.id.skc_form3_job);
+        goodsSpinner = (Spinner) view.findViewById(R.id.skc_form3_goods);
+        goodsAreaEdit = (EditText) view.findViewById(R.id.skc_form3_agriarea);
+        interestProductEdit = (EditText) view.findViewById(R.id.skc_form3_interestproduct);
+        agriTypeSpinner = (Spinner)view.findViewById(R.id.skc_form3_agritype);
+        harvestMethodSpinner = (Spinner)view.findViewById(R.id.skc_form3_harvestmethod);
+        riceMethodSpinner = (Spinner)view.findViewById(R.id.skc_form3_ricemethod);
+        incomeEdit = (EditText) view.findViewById(R.id.skc_form3_income);
+        menu1Button = (Button) view.findViewById(R.id.skc_form3_menu1);
+        menu2Button = (Button) view.findViewById(R.id.skc_form3_menu2);
+        addButton = (Button) view.findViewById(R.id.skc_form3_addbutton);
+        limitText = (TextView) view.findViewById(R.id.skc_form3_limit);
+    }
+
+    private void initMenu(View view){
+        menu1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(pageID){
+                    case 0:
+                        return;
+                    case 1:
+                        saveStatus();
+                        break;
+                    default:
+                        break;
+                }
+                pageID=0;
+                restoreStatus();
+            }
+        });
+
+        menu2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(pageID){
+                    case 0:
+                        saveStatus();
+                        break;
+                    case 1:
+                        return;
+                    default:
+                        break;
+                }
+                pageID=1;
+                restoreStatus();
+            }
+        });
+    }
+
+    private void initSpinner(View view){
+        ArrayAdapter<CharSequence> jobAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.job_choice, android.R.layout.simple_spinner_item);
+        jobAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        jobSpinner.setAdapter(jobAdapter);
+
+        ArrayAdapter<CharSequence> goodsAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.agri_choice, android.R.layout.simple_spinner_item);
+        goodsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        goodsSpinner.setAdapter(goodsAdapter);
+
+        ArrayAdapter<CharSequence> agriAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.agritype_choice, android.R.layout.simple_spinner_item);
+        agriAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        agriTypeSpinner.setAdapter(agriAdapter);
+
+        ArrayAdapter<CharSequence> harvestAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.harvest_choice, android.R.layout.simple_spinner_item);
+        harvestAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        harvestMethodSpinner.setAdapter(harvestAdapter);
+
+        ArrayAdapter<CharSequence> riceAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.rice_choice, android.R.layout.simple_spinner_item);
+        riceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        riceMethodSpinner.setAdapter(riceAdapter);
+    }
+
+    private void initInterestList(View view){
+        recyclerView = (RecyclerView) view.findViewById(R.id.skc_form3_listview);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        adapter = new listAdapter();
+        recyclerView.setAdapter(adapter);
+        final SwipeToDismissTouchListener<RecyclerViewAdapter> touchListener =
+                new SwipeToDismissTouchListener<>(
+                        new RecyclerViewAdapter(recyclerView),
+                        new SwipeToDismissTouchListener.DismissCallbacks<RecyclerViewAdapter>() {
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onPendingDismiss(RecyclerViewAdapter recyclerView, int position) {
+
+                            }
+
+                            @Override
+                            public void onDismiss(RecyclerViewAdapter view, final int position) {
+                                adapter.remove(position);
+                                limitText.setText(adapter.getItemCount()+"/5");
+                                jobSpinner.setEnabled(true);
+                                goodsSpinner.setEnabled(true);
+                                goodsAreaEdit.setEnabled(true);
+                                interestProductEdit.setEnabled(true);
+                                addButton.setBackgroundResource(R.drawable.button_shape_inactive);
+                                addButton.setEnabled(true);
+                            }
+                        });
+        touchListener.setDismissDelay(1500);
+        recyclerView.setOnTouchListener(touchListener);
+        recyclerView.setOnScrollListener((RecyclerView.OnScrollListener) touchListener.makeScrollListener());
+        recyclerView.addOnItemTouchListener(new SwipeableItemClickListener(view.getContext(),
+                new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        if (view.getId() == R.id.lead_txt_delete) {
+                            touchListener.processPendingDismisses();
+                        } else if (view.getId() == R.id.lead_txt_undo) {
+                            touchListener.undoPendingDismiss();
+                        }
+                    }
+                }));
+        limitText.setText(adapter.getItemCount()+"/5");
+    }
+
+    private void initAddButton(final View view){
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobStr = jobSpinner.getSelectedItem().toString();
+                String goodsStr = goodsSpinner.getSelectedItem().toString();
+                String agriArea = goodsAreaEdit.getText().toString();
+                String interestStr = interestProductEdit.getText().toString();
+                if(agriArea.length()>0&&interestStr.length()>0) {
+                    adapter.addData(new surveyInstance(jobStr, goodsStr, agriArea, interestStr));
+                    adapter.notifyDataSetChanged();
+                    goodsAreaEdit.setText("");
+                    interestProductEdit.setText("");
+                    goodsAreaEdit.setError(null);
+                    interestProductEdit.setError(null);
+                    limitText.setText(adapter.getItemCount() + "/5");
+                    if (adapter.getItemCount() == 5) {
+                        jobSpinner.setEnabled(false);
+                        goodsSpinner.setEnabled(false);
+                        goodsAreaEdit.setEnabled(false);
+                        interestProductEdit.setEnabled(false);
+                        addButton.setBackgroundResource(R.drawable.button_shape_inactive);
+                        addButton.setEnabled(false);
+                    }
+                }
+                else {
+                    if(agriArea.length()==0)
+                        goodsAreaEdit.setError("กรุณาใส่ข้อมูล");
+                    else
+                        goodsAreaEdit.setError(null);
+                    if(interestStr.length()==0)
+                        interestProductEdit.setError("กรุณาใส่ข้อมูล");
+                    else
+                        goodsAreaEdit.setError(null);
+                }
+            }
+        });
+    }
+
+    private void clearInfo(){
+        jobSpinner.setSelection(0);
+        goodsSpinner.setSelection(0);
+        agriTypeSpinner.setSelection(0);
+        harvestMethodSpinner.setSelection(0);
+        riceMethodSpinner.setSelection(0);
+        goodsAreaEdit.setText("");
+        interestProductEdit.setText("");
+        incomeEdit.setText("");
+        goodsAreaEdit.setError(null);
+        interestProductEdit.setError(null);
+        incomeEdit.setError(null);
+    }
+
+    private void saveStatus(){
+        pageList.get(pageID).setPage(jobSpinner, goodsSpinner, agriTypeSpinner, harvestMethodSpinner,
+                 riceMethodSpinner,  goodsAreaEdit,  interestProductEdit,  incomeEdit, adapter);
+        clearInfo();
+    }
+
+    private void restoreStatus(){
+        pageList.get(pageID).getPage(jobSpinner, goodsSpinner, agriTypeSpinner, harvestMethodSpinner,
+                riceMethodSpinner,  goodsAreaEdit,  interestProductEdit,  incomeEdit, adapter);
+    }
+
+    private class listAdapter extends RecyclerView.Adapter<listAdapter.MyViewHolder> {
+        ArrayList<surveyInstance> list;
+
+        public listAdapter() {
+            list = new ArrayList<surveyInstance>();
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.skc_form3_list_row, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.JobsText.setText(list.get(position).getJobs());
+            holder.GoodsText.setText(list.get(position).getGoods());
+            holder.agriAreaText.setText(list.get(position).getAgriArea());
+            holder.interestProductText.setText(list.get(position).getInterestProduct());
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public void remove(int position) {
+            list.remove(position);
+            notifyItemRemoved(position);
+        }
+
+        public void addData(surveyInstance item) {
+            list.add(item);
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView JobsText, GoodsText, agriAreaText, interestProductText;
+
+            MyViewHolder(View view) {
+                super(view);
+                JobsText = (TextView) view.findViewById(R.id.skc_list_job);
+                GoodsText = (TextView) view.findViewById(R.id.skc_list_goods);
+                agriAreaText = (TextView) view.findViewById(R.id.skc_list_agriarea);
+                interestProductText = (TextView) view.findViewById(R.id.skc_list_interest);
+            }
+        }
+    }
+
+    private class surveyInstance {
+        private String Jobs, Goods, agriArea, interestProduct;
+        public surveyInstance(String Jobs, String Goods, String agriArea, String interestProduct){
+            this.Jobs = Jobs;
+            this.Goods = Goods;
+            this.agriArea = agriArea;
+            this.interestProduct = interestProduct;
+        }
+
+        public String getJobs() {
+            return Jobs;
+        }
+
+        public String getGoods() {
+            return Goods;
+        }
+
+        public String getAgriArea() {
+            return agriArea;
+        }
+
+        public String getInterestProduct() {
+            return interestProduct;
+        }
+    }
+
+    private class PageInformation {
+        ArrayList<surveyInstance> savedList;
+        int jobChoice, goodsChoice, agriTypeChoice,
+                harvestChoice, riceChoice;
+        String agriAreaStr, interestProductStr, incomeStr;
+        PageInformation(){
+            savedList = new ArrayList<surveyInstance>();
+        }
+
+        public void setPage(Spinner jobSpinner,Spinner goodsSpinner,Spinner agriTypeSpinner,Spinner harvestMethodSpinner,
+                               Spinner riceMethodSpinner, EditText goodsAreaEdit, EditText interestProductEdit, EditText incomeEdit,
+                            listAdapter adapter){
+            jobChoice = jobSpinner.getSelectedItemPosition();
+            goodsChoice = goodsSpinner.getSelectedItemPosition();
+            agriTypeChoice = agriTypeSpinner.getSelectedItemPosition();
+            harvestChoice = harvestMethodSpinner.getSelectedItemPosition();
+            riceChoice = riceMethodSpinner.getSelectedItemPosition();
+            agriAreaStr = goodsAreaEdit.getText().toString();
+            interestProductStr = interestProductEdit.getText().toString();
+            incomeStr = incomeEdit.getText().toString();
+            savedList = adapter.list;
+        }
+
+        public void getPage(Spinner jobSpinner,Spinner goodsSpinner,Spinner agriTypeSpinner,Spinner harvestMethodSpinner,
+                            Spinner riceMethodSpinner, EditText goodsAreaEdit, EditText interestProductEdit, EditText incomeEdit, listAdapter adapter){
+            jobSpinner.setSelection(jobChoice);
+            goodsSpinner.setSelection(goodsChoice);
+            agriTypeSpinner.setSelection(agriTypeChoice);
+            harvestMethodSpinner.setSelection(harvestChoice);
+            riceMethodSpinner.setSelection(riceChoice);
+            goodsAreaEdit.setText(agriAreaStr);
+            interestProductEdit.setText(interestProductStr);
+            incomeEdit.setText(incomeStr);
+            adapter.list = savedList;
+            adapter.notifyDataSetChanged();
+        }
     }
 
 }
