287955f6ed2c934518d7765bf42ded7149e820a0
wh_orangester <w.h.orangester@gmail.com>
2016-07-26 12:20:33 +0700

    Minor Debug & Remove Some Logs

--- app/src/main/java/com/theteus/kubota/AccountModule/AccountDetailDetailsPart1.java
+++ app/src/main/java/com/theteus/kubota/AccountModule/AccountDetailDetailsPart1.java
@@ -6,7 +6,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
